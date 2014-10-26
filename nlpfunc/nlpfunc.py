cb4b861c6428d8f76bbc3a05e8c2553506f1e497
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2014-10-26 22:33:14 +0530

    started primitive viterbi

--- nlpfunc/nlpfunc.py
+++ nlpfunc/nlpfunc.py
@@ -632,3 +632,51 @@ class IBOGoodTuringSmoothing(object):
         for trigram in trigrams:
             probability *= self.perform_interpolation(trigram)
         return probability
+
+#Viterbi PoS Tagging
+class Viterbi(object):
+    
+    def __init__(self, input_params):
+        self.states = input_params['states']
+        self.observations = input_params['observations']
+        self.start_probability = input_params['start_probability']
+        self.transition_probability = input_params['transition_probability']
+        self.emission_probability = input_params['emission_probability']
+        #Executing Viterbi on the above parameters
+        self.viterbi()
+
+    def viterbi(self):
+        obs = self.observations
+        states = self.states
+        start_p = self.start_probability
+        trans_p = self.transition_probability
+        emit_p = self.emission_probability
+        
+        V = [{}]
+        path = {}
+     
+        # Initialize base cases (t == 0)
+        for y in states:
+            V[0][y] = start_p[y] * emit_p[y][obs[0]]
+            path[y] = [y]
+     
+        # Run Viterbi for t > 0
+        for t in range(1, len(obs)):
+            V.append({})
+            newpath = {}
+     
+            for y in states:
+                (prob, state) = max((V[t-1][y0] * trans_p[y0][y] * emit_p[y][obs[t]], y0) for y0 in states)
+                V[t][y] = prob
+                newpath[y] = path[state] + [y]
+     
+            # Don't need to remember the old paths
+            path = newpath
+        # if only one element is observed max is sought in the initialization values
+        n = 0
+        if len(obs)!=1:
+            n = t
+        (prob, state) = max((V[n][y], y) for y in states)
+        toReturnObj = (prob, path[state])
+        print toReturnObj
+        return toReturnObj
--- nlpfunc/program4.py
+++ nlpfunc/program4.py
@@ -7,6 +7,7 @@
 
 # program 4 - Viterbi PoS Tagging
 
+import nlpfunc
 import time
 #import os.path
 
@@ -14,64 +15,7 @@ print "Runnning Program 4...."
 starttime = time.asctime( time.localtime(time.time()) )
 print "Start Time :", starttime
 
-class Viterbi(object):
-    
-    def __init__(self, input_params):
-        self.states = input_params['states']
-        self.observations = input_params['observations']
-        self.start_probability = input_params['start_probability']
-        self.transition_probability = input_params['transition_probability']
-        self.emission_probability = input_params['emission_probability']
-        #Executing Viterbi on the above parameters
-        self.viterbi()
-
-    def viterbi(self):
-        obs = self.observations
-        states = self.states
-        start_p = self.start_probability
-        trans_p = self.transition_probability
-        emit_p = self.emission_probability
-        
-        V = [{}]
-        path = {}
-     
-        # Initialize base cases (t == 0)
-        for y in states:
-            V[0][y] = start_p[y] * emit_p[y][obs[0]]
-            path[y] = [y]
-     
-        # Run Viterbi for t > 0
-        for t in range(1, len(obs)):
-            V.append({})
-            newpath = {}
-     
-            for y in states:
-                (prob, state) = max((V[t-1][y0] * trans_p[y0][y] * emit_p[y][obs[t]], y0) for y0 in states)
-                V[t][y] = prob
-                newpath[y] = path[state] + [y]
-     
-            # Don't need to remember the old paths
-            path = newpath
-        # if only one element is observed max is sought in the initialization values
-        n = 0
-        if len(obs)!=1:
-            n = t
-        self.print_dptable(V)
-        (prob, state) = max((V[n][y], y) for y in states)
-        toReturnObj = (prob, path[state])
-        print toReturnObj
-        return toReturnObj
-
-    # Don't study this, it just prints a table of the steps.
-    def print_dptable(self, V):
-        s = "    " + " ".join(("%7d" % i) for i in range(len(V))) + "\n"
-        for y in V[0]:
-            s += "%.8s: " % y
-            s += " ".join("%.7s" % ("%f" % v[y]) for v in V)
-            s += "\n"
-        print(s)
-
-
+#The class for this program goes here......
 
 
 #Example Input Parameters for Viterbi algorithm
@@ -82,7 +26,7 @@ input_params['start_probability'] = {'Healthy': 0.6, 'Fever': 0.4}
 input_params['transition_probability'] = {'Healthy' : {'Healthy': 0.7, 'Fever': 0.3},'Fever' : {'Healthy': 0.4, 'Fever': 0.6}}
 input_params['emission_probability'] = {'Healthy' : {'normal': 0.5, 'cold': 0.4, 'dizzy': 0.1},'Fever' : {'normal': 0.1, 'cold': 0.3, 'dizzy': 0.6}}
 #Running Viterbi on the above example parameters
-vit = Viterbi(input_params)
+vit = nlpfunc.Viterbi(input_params)
         
 #For Assignment
 
